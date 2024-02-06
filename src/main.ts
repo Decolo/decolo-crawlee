c54f628a04f227c2bca4b80a7b2f1f59430f2fe2
Decolo <caicheng.fe.dev@gmail.com>
2024-02-06 23:36:23 +0800

    refactor: try scroll and fetch notes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,89 @@
 // For more information, see https://crawlee.dev/
-import { PlaywrightCrawler } from "crawlee";
+import { PlaywrightCrawler, log } from "crawlee";
 
 import { getAccessCode } from "./utils/question.js";
-import { XHS_COOKIE_KEY } from "./constants/index.js";
+import { DOMAIN, XHS_COOKIE_KEY } from "./constants/index.js";
+import { Page } from "playwright";
 
 const { db, mongoClient } = await import("./utils/mongo.js");
 const { redisClient } = await import("./utils/redis.js");
+
+let lastIndex = 0;
+const getContentAndInsertDB = async (page: Page) => {
+  const data = await page.$$eval("a.title", ($posts) => {
+    const scrapedData: { title: string; pathname: string }[] = [];
+
+    $posts.forEach(($post) => {
+      const pathname = $post.getAttribute("href");
+      const title = $post.querySelector("span")?.innerHTML || $post.innerHTML;
+
+      if (!pathname) return;
+
+      scrapedData.push({
+        title,
+        pathname: pathname,
+      });
+    });
+
+    return scrapedData;
+  });
+  
+  const items = data.slice(lastIndex);
+
+  lastIndex = data.length;
+
+  const contents = db.collection("contents");
+
+  const itemsToInsert = [];
+
+  for (const item of items) {
+    // 检查当前的 'href' 是否已经存在于集合中
+    const existingDocument = await contents.findOne({
+      href: `${DOMAIN}${item.pathname}`,
+    });
+
+    // 如果 'href' 不在集合中，那么添加这个数据到筛选后的数据列表
+    if (!existingDocument) {
+      itemsToInsert.push({
+        title: item.title,
+        href: `${DOMAIN}${item.pathname}`,
+      });
+    }
+  }
+  debugger;
+
+  if (!itemsToInsert.length) return;
+
+  try {
+    await contents.insertMany(
+      itemsToInsert,
+      { ordered: true }
+    );
+  } catch (e) {
+    log.error(`Insert data error: ${e}`);
+  }
+};
+
+const startFetchContent = async (page: Page) => {
+  await getContentAndInsertDB(page);
+
+  let _lastIndex = 0;
+  while (_lastIndex !== lastIndex) {
+    _lastIndex = lastIndex;
+
+    await page.evaluate(() => {
+      debugger
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+    debugger
+    
+    await page.waitForLoadState("networkidle");
+    await page.waitForTimeout(10000);
+    debugger;
+
+    await getContentAndInsertDB(page);
+  }
+};
 
 // PlaywrightCrawler crawls the web using a headless
 // browser controlled by the Playwright library.
@@ -33,7 +111,7 @@ const crawler = new PlaywrightCrawler({
     } catch (e) {
       console.error(e);
     }
-
+    debugger;
     const phone = await page.$(".phone");
 
     if (phone) {
@@ -60,7 +138,7 @@ const crawler = new PlaywrightCrawler({
       await signin?.click();
       await page.waitForLoadState("networkidle");
 
-      await page.waitForTimeout(10000);
+      await page.waitForTimeout(5000);
 
       // // page get cookie
       const cookies = await page.context().cookies();
@@ -70,33 +148,13 @@ const crawler = new PlaywrightCrawler({
       await redisClient.set(XHS_COOKIE_KEY, JSON.stringify(cookies));
     }
 
-    debugger;
-    const data = await page.$$eval("a.title", ($posts) => {
-      const scrapedData: { title: string; href: string }[] = [];
-
-      $posts.forEach(($post) => {
-        scrapedData.push({
-          title: $post.innerHTML,
-          href: $post.getAttribute("href") || "",
-        });
-      });
-
-      return scrapedData;
-    });
-
-    const titles = db.collection("titles");
-
-    try {
-      await titles.insertMany(data, { ordered: true });
-    } catch (e) {
-      console.error(e);
-    } finally {
-      await mongoClient.close();
-      await redisClient.quit();
-    }
+    await startFetchContent(page);
+    debugger
+    await mongoClient.close();
+    await redisClient.quit();
   },
   // Comment this option to scrape the full website.
-  maxRequestsPerCrawl: 2,
+  maxRequestsPerCrawl: 1,
   // Uncomment this option to see the browser window.
   // headless: false,
 });
