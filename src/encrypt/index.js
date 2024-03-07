d72ca946ccb32d3b8ad22482f02dd84ea92ae08a
Decolo <caicheng.fe.dev@gmail.com>
2024-03-08 02:20:15 +0800

    fix: improve crawler method by faking header (#4)

---
+++ src/encrypt/index.js
@@ -0,0 +1,774 @@
+var encrypt_lookup = ['Z', 'm', 's', 'e', 'r', 'b', 'B', 'o', 'H', 'Q', 't', 'N', 'P', '+', 'w', 'O', 'c', 'z', 'a', '/', 'L', 'p', 'n', 'g', 'G', '8', 'y', 'J', 'q', '4', '2', 'K', 'W', 'Y', 'j', '0', 'D', 'S', 'f', 'd', 'i', 'k', 'x', '3', 'V', 'T', '1', '6', 'I', 'l', 'U', 'A', 'F', 'M', '9', '7', 'h', 'E', 'C', 'v', 'u', 'R', 'X', '5']
+
+function a0_0x1131() {
+  var t = [
+    "xUKNL",
+    "jUrZI",
+    "rviFu",
+    "join",
+    "get",
+    "LjDtD",
+    "ZJHyP",
+    "wOmGY",
+    "enumera",
+    "aONWR",
+    "string",
+    "kQpMi",
+    "mZPJZ",
+    "Ysiay",
+    "czxKn",
+    "|5|6|4|",
+    "prototy",
+    "jklmnop",
+    "MuYbw",
+    "diDwk",
+    "TRFtx",
+    "drDHI",
+    "WLARA",
+    "xyz0123",
+    "asBytes",
+    "|6|0|1|",
+    "JOtEi",
+    "Oialn",
+    "OQrEi",
+    "uPnXq",
+    "VWXYZab",
+    "cIbFa",
+    "qYuta",
+    "QDOZZ",
+    "MahgM",
+    "iRXZq",
+    "22098XlFGYn",
+    "mmLKn",
+    "jMcIE",
+    "stringi",
+    "[object",
+    "nYqUQ",
+    "jSgjk",
+    "ucyEo",
+    "iewJI",
+    "vgTwl",
+    "DnNGR",
+    "oBytes",
+    "Xtwzk",
+    "aqlTy",
+    "JWnPK",
+    "1|0|2|4",
+    "qrstuvw",
+    "_gg",
+    "QLthP",
+    "FJIWy",
+    "yRnhISG",
+    "pjUsr",
+    "KAwuh",
+    "Thhoa",
+    "jarkJ",
+    "WjRNN",
+    "asStrin",
+    "x3VT16I",
+    "357835LaQWjW",
+    "SkIJl",
+    "size",
+    "iyorr",
+    "iHUeL",
+    "tTanW",
+    "tNusJ",
+    "NiSrP",
+    "eAt",
+    "TCArD",
+    "a2r1ZQo",
+    "iamspam",
+    "bOnfu",
+    "UNSKg",
+    "HIJKLMN",
+    "ZfMKC",
+    "bJhXU",
+    "zwAAc",
+    "JYxWY",
+    "lUAFM97",
+    "mwaRe",
+    "EzYWD",
+    "replace",
+    "uOtUJ",
+    "__esMod",
+    "ViQWI",
+    "aCMFL",
+    "EAKSd",
+    "ule",
+    "pqnFP",
+    "qYDsL",
+    "270726pnaYfG",
+    "glBZG",
+    "OwjMq",
+    "YGrjc",
+    "ZhAcd",
+    "JDqFL",
+    "456789+",
+    "kEjQs",
+    "lWhbD",
+    "OaLTI",
+    "dXlgm",
+    "cVte9UJ",
+    "ctor",
+    "hwomB",
+    "wDtJz",
+    "constru",
+    "ABHuC",
+    "zDETq",
+    "SYNeA",
+    "BGbij",
+    "ionFq",
+    "QzaNS",
+    "7|3|5|4",
+    "YlZGp",
+    "Bjniw",
+    "ZITuN",
+    "KPTzH",
+    "HrBeq",
+    "xobsT",
+    "kXJkC",
+    "QSrEZ",
+    "ENXtO",
+    "FYbRJ",
+    "wOcza/L",
+    "_hh",
+    "dVXMb",
+    "ppkua",
+    "WgamZ",
+    "HuwCW",
+    "362424fnLCuh",
+    "charCod",
+    "HhPqg",
+    "ODunI",
+    "eJzqq",
+    "charAt",
+    "JGAgI",
+    "ZmserbB",
+    "TURcG",
+    "WyrqF",
+    "iYJzH",
+    "VIwfH",
+    "tzzOB",
+    "YgiCH",
+    "byyMQ",
+    "ELxEv",
+    "0DSfdik",
+    "HRihr",
+    "_ii",
+    "aDsrp",
+    "ble",
+    "jTGtW",
+    "configu",
+    "cXiYW",
+    "56kSpAsC",
+    "158KIldlA",
+    "oHQtNP+",
+    "BHavO",
+    "PCIlh",
+    "QatIf",
+    "IKyqh",
+    "Words",
+    "Qwnrg",
+    "44lQAgNu",
+    "cdefghi",
+    "nTwxD",
+    "RHteb",
+    "coqPr",
+    "rJwmI",
+    "aBoeK",
+    "default",
+    "exports",
+    "rceYY",
+    "isArray",
+    "mdKKO",
+    "kzxWE",
+    "DeBtm",
+    "tjjUn",
+    "vJEcD",
+    "LpfE8xz",
+    "bin",
+    "HKazo",
+    "rable",
+    "call",
+    "wordsTo",
+    "zBiyt",
+    "GrsGL",
+    "fqulF",
+    "jevwl",
+    "mxfLj",
+    "xlUnt",
+    "q42KWYj",
+    "endian",
+    "eEqDc",
+    "oyGAZ",
+    "bytesTo",
+    "OzjuJ",
+    "IfwWq",
+    "ize",
+    "6648810piiNEz",
+    "lTHdy",
+    "vDLZJ",
+    "stringT",
+    "A4NjFqY",
+    "GkjTz",
+    "eooJA",
+    "substr",
+    "veNiI",
+    "LYfDp",
+    "ljKsP",
+    "jJYWG",
+    "bcYAf",
+    "srikB",
+    "utf8",
+    "qTbeY",
+    "yqRzd",
+    "|3|5",
+    "bjbAy",
+    " Array]",
+    "rMbXP",
+    "u5wPHsO",
+    "test",
+    "gMIMC",
+    "Deyqv",
+    " argume",
+    "ABCDEFG",
+    "undefin",
+    "split",
+    "QTlsj",
+    "_isBuff",
+    "OPQRSTU",
+    "Illegal",
+    "loSen",
+    "navigat",
+    "ObwNo",
+    "qPbcq",
+    "7182692QogvXX",
+    "tvqSn",
+    "DGptJ",
+    "HhTfW",
+    "avIYx",
+    "defineP",
+    "PFQbW",
+    "CjFyM",
+    "toStrin",
+    "yMWXS",
+    "yMyOy",
+    "0XTdDgM",
+    "eXkru",
+    "_blocks",
+    "indexOf",
+    "mbBQr",
+    "lBuRH",
+    "HzGjH",
+    "HNErV",
+    "mEokX",
+    "userAge",
+    "UpmtD",
+    "sgomx",
+    "KDfKS",
+    "OTbSq",
+    "lxMGW",
+    "0|3|2|1",
+    "dfWyB",
+    "lWzAd",
+    "eyXTL",
+    "5624qreyZK",
+    "pow",
+    "IJstz",
+    "LMlMB",
+    "INlwI",
+    "lRulU",
+    "TCgZh",
+    "_digest",
+    "UBhIl",
+    "fLtZZ",
+    "FYSKq",
+    "2|8|0",
+    "IoCeZ",
+    " Object",
+    "UuTvI",
+    "lNKLD",
+    "String",
+    "Bytes",
+    "rBVvW",
+    "KblCWi+",
+    "pRaIH",
+    "roperty",
+    "vTINI",
+    "atLE",
+    "functio",
+    "Udqoy",
+    "nt ",
+    "htSWx",
+    "hEwRK",
+    "encodin",
+    "sCSVK",
+    "VuAZF",
+    "xeIIy",
+    "RBjMb",
+    "taTrq",
+    "vDLFJ",
+    "bPkya",
+    "HzimH",
+    "nCffO",
+    "BWbtU",
+    "2|8",
+    "slice",
+    "lxMGQ",
+    "tTiwe",
+    "JDhJB",
+    "rCode",
+    "gNDzY",
+    "wJkyu",
+    "cCZFe",
+    "RNGSl",
+    "floor",
+    "clYIu",
+    "vLiwz",
+    "BiNSE",
+    "MtYWB",
+    "fromCha",
+    "StNOc",
+    "|7|5|3|",
+    "9|1|4|6",
+    "length",
+    "UNYAE",
+    "pngG8yJ",
+    "hasOwnP",
+    "pYeWu",
+    "wTjkk",
+    "Bvk6/7=",
+    "KTmgk",
+    "bIGxm",
+    "readFlo",
+    "LFZch",
+    "_ff",
+    "1|3|4|2",
+    "binary",
+    "LLdJZ",
+    "ZofOU",
+    "6399uFPxTQ",
+    "push",
+    "YntPT",
+    "kSGXO",
+    "random",
+    "HfpCU",
+    "hECvuRX",
+    "getTime",
+    "iwSyV",
+    "alert",
+    "LKMcb",
+    "DJVdg",
+    "Hex",
+    "URzKO",
+    "CxjtF",
+    "ZVOCs",
+    "isBuffe",
+    "vGpbT",
+    "rotl",
+    "udFrB",
+    "CnbsH",
+    "crLST",
+  ];
+  return (a0_0x1131 = function () {
+    return t;
+  })();
+}
+
+function a0_0x3693(t, e) {
+  var r = a0_0x1131();
+  return (a0_0x3693 = function (t, e) {
+    return r[(t -= 131)];
+  })(t, e);
+}
+
+function a0_0x10f4ac(t, e) {
+  return a0_0x3693(e - -570, t);
+}
+
+function encrypt_encodeChunk(t, e, r) {
+    var n, o = 165, i = 246, a = 205, u = 353, s = 162, c = 17, l = 351, f = 191, p = 139, h = 79, d = 86, v = 233, g = 270, m = 166, y = {
+        hwomB: function(t, e) {
+            return t < e
+        },
+        iHUeL: function(t, e) {
+            return t & e
+        },
+        ELxEv: function(t, e) {
+            return t << e
+        },
+        lBuRH: function(t, e) {
+            return t << e
+        },
+        SkIJl: function(t, e) {
+            return t & e
+        },
+        JYxWY: function(t, e) {
+            return t + e
+        },
+        CxjtF: function(t, e) {
+            return t(e)
+        }
+    }, w = [];
+    function b(t, e) {
+        return a0_0x10f4ac(t, e - m)
+    }
+    for (var _ = e; y[b(-63, -o)](_, r); _ += 3)
+        n = y[b(-i, -a)](y[b(-166, -124)](t[_], 16), 16711680) + y[b(-u, -205)](y[b(s, -c)](t[_ + 1], 8), 65280) + y[b(-l, -208)](t[y[b(-350, -f)](_, 2)], 255),
+        w[b(p, 73)](y[b(h, d)](encrypt_tripletToBase64, n));
+    return w[b(-v, -g)]("")
+}
+
+function encrypt_tripletToBase64(t) {
+    var e = 11
+      , r = 15
+      , n = 199
+      , o = 34
+      , i = 4
+      , a = 102
+      , u = 276
+      , s = 205
+      , c = 218
+      , l = 11
+      , f = 115
+      , p = 34
+      , h = 161
+      , d = 123
+      , v = 335
+      , g = {};
+    function m(t, e) {
+        return a0_0x10f4ac(e, t - v)
+    }
+    g[m(205, 328)] = function(t, e) {
+        return t + e
+    }
+    ,
+    g[m(e, 53)] = function(t, e) {
+        return t >> e
+    }
+    ,
+    g[m(r, n)] = function(t, e) {
+        return t & e
+    }
+    ,
+    g[m(o, i)] = function(t, e) {
+        return t >> e
+    }
+    ,
+    g[m(-a, -u)] = function(t, e) {
+        return t & e
+    }
+    ;
+    var y = g;
+    return y[m(s, c)](encrypt_lookup[63 & y[m(l, -75)](t, 18)], encrypt_lookup[y[m(r, f)](y[m(p, h)](t, 12), 63)]) + encrypt_lookup[t >> 6 & 63] + encrypt_lookup[y[m(-a, -d)](t, 63)]
+}
+
+export var encrypt_mcr = (function (t) {
+  var e = 67,
+    r = 15,
+    n = 164,
+    o = 126,
+    i = 137,
+    a = 39,
+    u = 176,
+    s = 72,
+    c = 56,
+    l = 21,
+    f = 35,
+    p = 34,
+    h = 35,
+    d = 18,
+    v = 25,
+    g = 185,
+    m = 1149,
+    y = 744,
+    w = 1295,
+    b = 1248,
+    _ = 1310,
+    E = 1096,
+    x = 1166,
+    T = 1095,
+    k = 1196,
+    S = 1180,
+    A = 1039,
+    L = 976,
+    R = 1347,
+    I = 1117,
+    O = 1168,
+    C = 1233,
+    N = 1157,
+    P = 1006,
+    B = 1122,
+    M = 1277,
+    j = 1288,
+    F = 1271,
+    D = 986,
+    U = 162,
+    q = {};
+  function G(t, e) {
+    return a0_0x10f4ac(e, t - U);
+  }
+  (q[G(-73, -66)] = function (t, e) {
+    return t === e;
+  }),
+    (q[G(e, 186)] = function (t, e) {
+      return t < e;
+    }),
+    (q[G(-r, -n)] = function (t, e) {
+      return t ^ e;
+    }),
+    (q[G(r, -o)] = function (t, e) {
+      return t & e;
+    }),
+    (q[G(-i, -a)] = function (t, e) {
+      return t < e;
+    }),
+    (q[G(-175, -u)] = function (t, e) {
+      return t ^ e;
+    }),
+    (q[G(-59, s)] = function (t, e) {
+      return t ^ e;
+    }),
+    (q[G(-c, -l)] = function (t, e) {
+      return t >>> e;
+    }),
+    (q[G(f, p)] = function (t, e) {
+      return t >>> e;
+    });
+  for (
+    var H, V, W = q, z = 3988292384, X = 256, Y = [];
+    X--;
+    Y[X] = W[G(h, -66)](H, 0)
+  )
+    for (V = 8, H = X; V--; )
+      H = W[G(r, d)](H, 1) ? W[G(35, v)](H, 1) ^ z : W[G(h, g)](H, 1);
+  return function (t) {
+    function e(t, e) {
+      return G(e - 1181, t);
+    }
+    if (
+      W[e(m, 1108)](
+        (0,
+        function (t) {
+          return typeof t;
+        })(t),
+        e(y, 914)
+      )
+    ) {
+      for (var r = 0, n = -1; W[e(w, b)](r, t[e(_, 1233)]); ++r)
+        n = W[e(E, x)](
+          Y[W[e(T, k)](n, 255) ^ t[e(S, A) + e(1022, L)](r)],
+          n >>> 8
+        );
+      return W[e(R, 1166)](n, -1) ^ z;
+    }
+    for (r = 0, n = -1; W[e(I, 1044)](r, t[e(O, C)]); ++r)
+      n = W[e(N, P)](
+        Y[W[e(1229, B)](W[e(M, k)](n, 255), t[r])],
+        W[e(j, 1125)](n, 8)
+      );
+    return W[e(F, B)](W[e(D, 1122)](n, -1), z);
+  };
+})();
+
+export function encrypt_encodeUtf8(t) {
+  var e = 185,
+    r = 410,
+    n = 480,
+    o = 222,
+    i = 194,
+    a = 165,
+    u = 147,
+    s = 290,
+    c = 460,
+    l = 472,
+    f = 497,
+    p = 462,
+    h = 286,
+    d = 209,
+    v = 223,
+    g = 590,
+    m = {
+      bIGxm: function (t, e) {
+        return t(e);
+      },
+      MahgM: function (t, e) {
+        return t < e;
+      },
+      czxKn: function (t, e) {
+        return t === e;
+      },
+      clYIu: function (t, e) {
+        return t + e;
+      },
+    },
+    y = m[b(477, 488)](encodeURIComponent, t),
+    w = [];
+  function b(t, e) {
+    return a0_0x10f4ac(t, e - g);
+  }
+  for (var _ = 0; m[b(333, e)](_, y[b(r, n)]); _++) {
+    var E = y[b(o, 290)](_);
+    if (m[b(i, a)](E, "%")) {
+      var x =
+          y[b(u, s)](m[b(574, 472)](_, 1)) + y[b(c, 290)](m[b(605, l)](_, 2)),
+        T = parseInt(x, 16);
+      w[b(592, f)](T), (_ += 2);
+    } else w[b(p, f)](E[b(217, h) + b(d, v)](0));
+  }
+  return w;
+}
+
+export function encrypt_b64Encode(t) {
+  var e = 664,
+    r = 634,
+    n = 448,
+    o = 599,
+    i = 315,
+    a = 416,
+    u = 512,
+    s = 361,
+    c = 406,
+    l = 487,
+    f = 496,
+    p = 333,
+    h = 630,
+    d = 639,
+    v = 548,
+    g = 582,
+    m = 447,
+    y = 468,
+    w = 375,
+    b = 331,
+    _ = 149,
+    E = 382,
+    x = 265,
+    T = 625,
+    k = 570,
+    S = 551,
+    A = 582,
+    L = 581,
+    R = 638,
+    I = 618,
+    O = 606,
+    C = 429,
+    N = 651,
+    P = 667,
+    B = 817,
+    M = 333,
+    j = 567,
+    F = 747,
+    D = 561,
+    U = 570,
+    q = 676,
+    G = 840,
+    H = 240,
+    V = {
+      udFrB: function (t, e) {
+        return t % e;
+      },
+      cCZFe: function (t, e) {
+        return t === e;
+      },
+      jevwl: function (t, e) {
+        return t - e;
+      },
+      aqlTy: function (t, e) {
+        return t + e;
+      },
+      rceYY: function (t, e) {
+        return t >> e;
+      },
+      OwjMq: function (t, e) {
+        return t & e;
+      },
+      kSGXO: function (t, e) {
+        return t << e;
+      },
+      veNiI: function (t, e) {
+        return t === e;
+      },
+      QLthP: function (t, e) {
+        return t + e;
+      },
+      wDtJz: function (t, e) {
+        return t + e;
+      },
+      nYqUQ: function (t, e) {
+        return t & e;
+      },
+      TCArD: function (t, e) {
+        return t << e;
+      },
+      RHteb: function (t, e) {
+        return t - e;
+      },
+      mZPJZ: function (t, e) {
+        return t < e;
+      },
+      zDETq: function (t, e, r, n) {
+        return t(e, r, n);
+      },
+      YlZGp: function (t, e) {
+        return t > e;
+      },
+    };
+  function W(t, e) {
+    return a0_0x10f4ac(e, t - -H);
+  }
+  for (var z = (W(-413, -442) + W(-e, -r) + "7")[W(-n, -o)]("|"), X = 0; ; ) {
+    switch (z[X++]) {
+      case "0":
+        var Y;
+        continue;
+      case "1":
+        var K = [];
+        continue;
+      case "2":
+        var J = V[W(-i, -a)]($, 3);
+        continue;
+      case "3":
+        var $ = t[W(-350, -u)];
+        continue;
+      case "4":
+        V[W(-s, -c)](J, 1)
+          ? ((Y = t[V[W(-l, -f)]($, 1)]),
+            K[W(-p, -346)](
+              V[W(-h, -d)](
+                encrypt_lookup[V[W(-503, -v)](Y, 2)] +
+                  encrypt_lookup[V[W(-g, -741)](V[W(-331, -m)](Y, 4), 63)],
+                "=="
+              )
+            ))
+          : V[W(-y, -w)](J, 2) &&
+            ((Y = V[W(-b, -_)](t[$ - 2], 8) + t[V[W(-l, -E)]($, 1)]),
+            K[W(-333, -x)](
+              V[W(-T, -505)](
+                V[W(-k, -S)](
+                  encrypt_lookup[Y >> 10],
+                  encrypt_lookup[V[W(-A, -L)](Y >> 4, 63)]
+                ) + encrypt_lookup[V[W(-R, -I)](V[W(-O, -C)](Y, 2), 63)],
+                "="
+              )
+            ));
+        continue;
+      case "5":
+        var Q = 16383;
+        continue;
+      case "6":
+        for (var Z = 0, tt = V[W(-509, -N)]($, J); V[W(-P, -B)](Z, tt); Z += Q)
+          K[W(-M, -153)](
+            V[W(-j, -F)](
+              encrypt_encodeChunk,
+              t,
+              Z,
+              V[W(-D, -413)](Z + Q, tt) ? tt : V[W(-U, -501)](Z, Q)
+            )
+          );
+        continue;
+      case "7":
+        return K[W(-q, -G)]("");
+    }
+    break;
+  }
+}
